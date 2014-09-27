9ca6a9f7bd3951705481ebd153a13d462491c40f
10to7 <elaine.github@10to7.com>
2014-09-28 00:28:14 +0100

    removed sillyness.

--- sw/hydroponics/hydroponics.ino
+++ sw/hydroponics/hydroponics.ino
@@ -40,163 +40,163 @@ int lastLightSwitchState = 0;
 int notEnoughWater = 0;
 
 void setup(void) {
-  Serial.begin(9600);
-  
-  setupRadio();
-  if(getDate()) {
-   // create the alarms 
-    Alarm.alarmRepeat(6,0,0, MorningAlarm);  // 0600 every day
-    Alarm.alarmRepeat(22,00,0, EveningAlarm);  // 2200 every day 
-    if(hour() > 6 || hour() < 22) {
-      MorningAlarm();
-    } else {
-      EveningAlarm();
-    }
-  }
-  
-  pinMode(pumpSwitch, INPUT);
-  pinMode(lightSwitch, INPUT);
-  pinMode(lowWater, INPUT);
-  pinMode(tankOne, INPUT);
-  pinMode(tankTwo, INPUT);
-  
-  pinMode(pumpPin, OUTPUT);
-  pinMode(lightPin, OUTPUT);
-  pinMode(errorLED, OUTPUT);
-  pinMode(blinkLED, OUTPUT);
-  
-  digitalWrite(pumpSwitch, HIGH);
-  digitalWrite(lightSwitch, HIGH);
-  digitalWrite(lowWater, HIGH);
-  digitalWrite(tankOne, HIGH);
-  digitalWrite(tankTwo, HIGH);
-  digitalWrite(blinkLED, HIGH);
-  
-  digitalWrite(lightPin, HIGH);
-  digitalWrite(pumpPin, HIGH);
+	Serial.begin(9600);
+	
+	setupRadio();
+	if(getDate()) {
+		// create the alarms
+		Alarm.alarmRepeat(6,0,0, MorningAlarm);  // 0600 every day
+		Alarm.alarmRepeat(22,00,0, EveningAlarm);  // 2200 every day
+		if(hour() > 6 || hour() < 22) {
+			MorningAlarm();
+			} else {
+			EveningAlarm();
+		}
+	}
+	
+	pinMode(pumpSwitch, INPUT);
+	pinMode(lightSwitch, INPUT);
+	pinMode(lowWater, INPUT);
+	pinMode(tankOne, INPUT);
+	pinMode(tankTwo, INPUT);
+	
+	pinMode(pumpPin, OUTPUT);
+	pinMode(lightPin, OUTPUT);
+	pinMode(errorLED, OUTPUT);
+	pinMode(blinkLED, OUTPUT);
+	
+	digitalWrite(pumpSwitch, HIGH);
+	digitalWrite(lightSwitch, HIGH);
+	digitalWrite(lowWater, HIGH);
+	digitalWrite(tankOne, HIGH);
+	digitalWrite(tankTwo, HIGH);
+	digitalWrite(blinkLED, HIGH);
+	
+	digitalWrite(lightPin, HIGH);
+	digitalWrite(pumpPin, HIGH);
 }
 
 void loop(void) {
-  pumpProcesses();
-  lightProcess();
-  Alarm.delay(100);
-  
+	pumpProcesses();
+	lightProcess();
+	Alarm.delay(100);
+	
 }
 
 void MorningAlarm() {
-  Serial.println("Morning Alarm");
-  digitalWrite(lightPin, LOW);
-  digitalWrite(pumpPin, LOW);
-  // make sure we have the right time;
-  getDate();
+	Serial.println("Morning Alarm");
+	digitalWrite(lightPin, LOW);
+	digitalWrite(pumpPin, LOW);
+	// make sure we have the right time;
+	getDate();
 }
 
 void EveningAlarm() {
-  Serial.println("Evening Alarm");
-  digitalWrite(lightPin, HIGH);
-  // make sure we have the right time;
-  getDate();
+	Serial.println("Evening Alarm");
+	digitalWrite(lightPin, HIGH);
+	// make sure we have the right time;
+	getDate();
 }
 
 void setupRadio() {
-  radio.begin();
+	radio.begin();
 
-  // Enable this seems to work better
-  radio.enableDynamicPayloads();
+	// Enable this seems to work better
+	radio.enableDynamicPayloads();
 
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setChannel(76);
-  radio.setRetries(15,15);
+	radio.setDataRate(RF24_1MBPS);
+	radio.setPALevel(RF24_PA_MAX);
+	radio.setChannel(76);
+	radio.setRetries(15,15);
 
-  radio.openWritingPipe(pipes[0]); 
-  radio.openReadingPipe(1,pipes[1]); 
+	radio.openWritingPipe(pipes[0]);
+	radio.openReadingPipe(1,pipes[1]);
 
-      
-  // Send only, ignore listening mode
-  //radio.startListening();
+	
+	// Send only, ignore listening mode
+	//radio.startListening();
 
-  // Dump the configuration of the rf unit for debugging
-  radio.printDetails(); 
+	// Dump the configuration of the rf unit for debugging
+	radio.printDetails();
 }
 
 boolean getDate() {
-  char outBuffer[32]=""; // Clear the outBuffer before every loop
-  unsigned long send_time, rtt = 0;
-  bool timeout=0;
-  send_time = millis();
-  strcat(outBuffer,"Get Time");
-  // Stop listening and write to radio 
-  radio.stopListening();
-    
-  // Send to hub
-  digitalWrite(blinkLED, LOW);
-  if ( radio.write( outBuffer, strlen(outBuffer)) ) {
-    Serial.println("Send Time Request: successful\n\r"); 
-  }
-  else {
-    Serial.println("Send Time Request: failed\n\r"); 
-  }
-  digitalWrite(blinkLED, HIGH);
-  
-  radio.startListening();
-  delay(20);
-  
-  while ( radio.available() && !timeout ) {
-         uint8_t len = radio.getDynamicPayloadSize();
-         radio.read( receivePayload, len); 
-         
-         receivePayload[len] = 0;
-         if (len == 19) {
-           setTimeFromChar(receivePayload);
-                  
-         rtt = millis() - send_time;
-         Serial.println("Time set");
-         
-           return true;
-           
-         } else {
-           Serial.print("Send Time Request: unexpected response: ");
-           Serial.println(receivePayload);
-         }
-         //Serial.print("inBuffer --> rtt: ");
-         //Serial.println(rtt);          
-    
-    // Check for timeout and exit the while loop
-    if ( millis() - send_time > radio.getMaxTimeout() ) {
-         Serial.println("Timeout!!!");
-         timeout = 1;
-     }   
-     delay(10);
-   } // End while
-  
-  return false;   
+	char outBuffer[32]=""; // Clear the outBuffer before every loop
+	unsigned long send_time, rtt = 0;
+	bool timeout=0;
+	send_time = millis();
+	strcat(outBuffer,"Get Time");
+	// Stop listening and write to radio
+	radio.stopListening();
+	
+	// Send to hub
+	digitalWrite(blinkLED, LOW);
+	if ( radio.write( outBuffer, strlen(outBuffer)) ) {
+		Serial.println("Send Time Request: successful\n\r");
+	}
+	else {
+		Serial.println("Send Time Request: failed\n\r");
+	}
+	digitalWrite(blinkLED, HIGH);
+	
+	radio.startListening();
+	delay(20);
+	
+	while ( radio.available() && !timeout ) {
+		uint8_t len = radio.getDynamicPayloadSize();
+		radio.read( receivePayload, len);
+		
+		receivePayload[len] = 0;
+		if (len == 19) {
+			setTimeFromChar(receivePayload);
+			
+			rtt = millis() - send_time;
+			Serial.println("Time set");
+			
+			return true;
+			
+			} else {
+			Serial.print("Send Time Request: unexpected response: ");
+			Serial.println(receivePayload);
+		}
+		//Serial.print("inBuffer --> rtt: ");
+		//Serial.println(rtt);
+		
+		// Check for timeout and exit the while loop
+		if ( millis() - send_time > radio.getMaxTimeout() ) {
+			Serial.println("Timeout!!!");
+			timeout = 1;
+		}
+		delay(10);
+	} // End while
+	
+	return false;
 }
 
 void setTimeFromChar(char* date) {
-  char cHour[] = { date[11], date[12], 0 };
-  char cMin[] = { date[14], date[15], 0 };
-  char cSec[] = { date[17], date[18], 0 };
-  char cDay[] = { date[8], date[9], 0 };
-  char cMonth[] = { date[5], date[6], 0 };
-  char cYear[] = { date[2], date[3], 0 };
-  
-  Serial.println(date);
-  
-  Serial.print(" Hour : ");
-  Serial.print(cHour);
-  Serial.print(" Min : ");
-  Serial.print(cMin);
-  Serial.print(" Sec : ");
-  Serial.print(cSec);
-  Serial.print(" Day :  ");
-  Serial.print(cDay);
-  Serial.print(" Month : ");
-  Serial.print(cMonth);
-  Serial.print(" Year : ");
-  Serial.println(cYear);
-  
-  setTime(atoi(cHour), atoi(cMin), atoi(cSec), atoi(cDay), atoi(cMonth), atoi(cYear));
+	char cHour[] = { date[11], date[12], 0 };
+	char cMin[] = { date[14], date[15], 0 };
+	char cSec[] = { date[17], date[18], 0 };
+	char cDay[] = { date[8], date[9], 0 };
+	char cMonth[] = { date[5], date[6], 0 };
+	char cYear[] = { date[2], date[3], 0 };
+	
+	Serial.println(date);
+	
+	Serial.print(" Hour : ");
+	Serial.print(cHour);
+	Serial.print(" Min : ");
+	Serial.print(cMin);
+	Serial.print(" Sec : ");
+	Serial.print(cSec);
+	Serial.print(" Day :  ");
+	Serial.print(cDay);
+	Serial.print(" Month : ");
+	Serial.print(cMonth);
+	Serial.print(" Year : ");
+	Serial.println(cYear);
+	
+	setTime(atoi(cHour), atoi(cMin), atoi(cSec), atoi(cDay), atoi(cMonth), atoi(cYear));
 }
 
 void lightProcess(){
@@ -204,108 +204,104 @@ void lightProcess(){
 	int lightChanged = (lastLightSwitchState != digitalRead(lightSwitch));
 	lastLightSwitchState = digitalRead(lightSwitch);
 	
-	if(checkTankState() == true) {
-		if(digitalRead(lightPin) == HIGH) {
-			if(digitalRead(lightSwitch) == LOW
-			&& lightChanged == HIGH) {
-				digitalWrite(lightPin, LOW);
-			}
-			} else {
-			if (lightChanged == HIGH
-			&& lastLightSwitchState == LOW) {
-				digitalWrite(lightPin, HIGH);
-			}
+	if(digitalRead(lightPin) == HIGH) {
+		if(digitalRead(lightSwitch) == LOW
+		&& lightChanged == HIGH) {
+			digitalWrite(lightPin, LOW);
 		}
 		} else {
-		digitalWrite(lightPin, HIGH);
+		if (lightChanged == HIGH
+		&& lastLightSwitchState == LOW) {
+			digitalWrite(lightPin, HIGH);
+		}
 	}
 }
 void pumpProcesses() {
-  pumpState = digitalRead(pumpPin);
-  
-  tankOneState = digitalRead(tankOne);
-  tankTwoState = digitalRead(tankTwo);
-  lowWaterState = digitalRead(lowWater);
-  
-  int pumpChanged = (lastpumpSwitchState != digitalRead(pumpSwitch));
-  lastpumpSwitchState = digitalRead(pumpSwitch);
-  /*
-  Serial.print("Switch state : ");
-  Serial.print(pumpState);
-  Serial.print(lightState);
-  Serial.print(tankOneState);
-  Serial.print(tankTwoState);
-  Serial.print(lowWaterState);
-  Serial.println(pumpChanged);
-  */
-  if(checkTankState() == true) {
-    if(digitalRead(pumpPin) == HIGH) {
-      if(digitalRead(pumpSwitch) == LOW 
-      && pumpChanged == HIGH) {
-        digitalWrite(pumpPin, LOW);
-      }
-    } else {
-      if (pumpChanged == HIGH 
-      && lastpumpSwitchState == LOW) {
-        digitalWrite(pumpPin, HIGH);
-      }
-    }
-  } else {
-    digitalWrite(pumpPin, HIGH);
-  }
-  
-  if(notEnoughWater == 1) {
-    digitalWrite(errorLED, !digitalRead(errorLED));
-  } else {
-    digitalWrite(errorLED, HIGH);
-  }
-  
+	pumpState = digitalRead(pumpPin);
+	
+	tankOneState = digitalRead(tankOne);
+	tankTwoState = digitalRead(tankTwo);
+	lowWaterState = digitalRead(lowWater);
+	
+	int pumpChanged = (lastpumpSwitchState != digitalRead(pumpSwitch));
+	lastpumpSwitchState = digitalRead(pumpSwitch);
+	/*
+	Serial.print("Switch state : ");
+	Serial.print(pumpState);
+	Serial.print(lightState);
+	Serial.print(tankOneState);
+	Serial.print(tankTwoState);
+	Serial.print(lowWaterState);
+	Serial.println(pumpChanged);
+	*/
+	if(checkTankState() == true) {
+		if(digitalRead(pumpPin) == HIGH) {
+			if(digitalRead(pumpSwitch) == LOW
+			&& pumpChanged == HIGH) {
+				digitalWrite(pumpPin, LOW);
+			}
+			} else {
+			if (pumpChanged == HIGH
+			&& lastpumpSwitchState == LOW) {
+				digitalWrite(pumpPin, HIGH);
+			}
+		}
+		} else {
+		digitalWrite(pumpPin, HIGH);
+	}
+	
+	if(notEnoughWater == 1) {
+		digitalWrite(errorLED, !digitalRead(errorLED));
+		} else {
+		digitalWrite(errorLED, HIGH);
+	}
+	
 }
 
 boolean checkTankState(){
-  if(lowWaterState == HIGH) {
-    if(digitalRead(pumpPin) == LOW) {
-      Serial.println("WARNING: Not enough water to complete the pump cycle.");
-    }
-    notEnoughWater = 1;
-    return false;
-  }
-  
-  if(tankOneState == LOW 
-  || tankTwoState == LOW) {
-    notEnoughWater = 0;
-    if(digitalRead(pumpPin) == LOW) {
-      Serial.println("Watercycle complete.");
-    }
-    return false;
-  }
-  return true;
+	if(lowWaterState == HIGH) {
+		if(digitalRead(pumpPin) == LOW) {
+			Serial.println("WARNING: Not enough water to complete the pump cycle.");
+		}
+		notEnoughWater = 1;
+		return false;
+	}
+	
+	if(tankOneState == LOW
+	|| tankTwoState == LOW) {
+		notEnoughWater = 0;
+		if(digitalRead(pumpPin) == LOW) {
+			Serial.println("Watercycle complete.");
+		}
+		return false;
+	}
+	return true;
 }
 /*
-  float reading;
-  float ref;
-  
-  reading = analogRead(A0);
-  ref = analogRead(A1);
-  Serial.print("Analog read: ");
-  Serial.print(reading);
-  Serial.print(" : ");
-  Serial.println(ref);
-  
-  reading = (1023 / reading) -1;
-  reading = 540 / reading;
-  ref = (1023 / ref) -1;
-  ref = 540 / ref;
-  
-  Serial.print("Sensor resistance: ");
-  Serial.print(reading);
-  Serial.print(" : ");
-  Serial.println(ref);
-  
-  reading = reading - 300;
-  reading = 22.8-  (reading / 56) ;
-  Serial.print("Distance in cm: ");
-  Serial.println(reading);
-  
-  delay(1000);
+float reading;
+float ref;
+
+reading = analogRead(A0);
+ref = analogRead(A1);
+Serial.print("Analog read: ");
+Serial.print(reading);
+Serial.print(" : ");
+Serial.println(ref);
+
+reading = (1023 / reading) -1;
+reading = 540 / reading;
+ref = (1023 / ref) -1;
+ref = 540 / ref;
+
+Serial.print("Sensor resistance: ");
+Serial.print(reading);
+Serial.print(" : ");
+Serial.println(ref);
+
+reading = reading - 300;
+reading = 22.8-  (reading / 56) ;
+Serial.print("Distance in cm: ");
+Serial.println(reading);
+
+delay(1000);
 */
